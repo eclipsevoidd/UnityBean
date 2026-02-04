62fb01bb6109a6e83326af7c9588f981b2cd2ec8
eclipsevoidd <joeplays359@gmail.com>
2026-02-04 10:13:26 +0200

    Nodrošināta kanālu pārslēgšana.

--- Assets/Scripts/TVScript.cs
+++ Assets/Scripts/TVScript.cs
@@ -1,33 +1,66 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class TVScript : MonoBehaviour
 {
     public GameObject tvScreen1;
     public GameObject tvScreen2;
-    public TMP_Text buttonText;
 
-    public AudioSource AudioSource;
+    public TMP_Text buttonText;
+    public GameObject channelButton;
+
+    public AudioSource audioSource;
     public AudioClip tvSound;
 
     public bool isOn = false;
 
+    void Start()
+    {
+        channelButtonVisible();
+    }
+
     public void ToggleTV()
     {
-    AudioSource.PlayOneShot(tvSound);
-        if (!isOn)
+        audioSource.PlayOneShot(tvSound);
+        isOn = !isOn;
+
+        if (isOn)
         {
             tvScreen1.SetActive(true);
+            tvScreen2.SetActive(false);
             buttonText.text = "Izslēgt televizoru";
-            isOn = true;
-    }
+        }
         else
         {
             tvScreen1.SetActive(false);
+            tvScreen2.SetActive(false);
             buttonText.text = "Ieslēgt televizoru";
-            isOn = false;
+        }
+
+        channelButtonVisible();
+    }
+
+    public void SwitchChannel()
+    {
+        if (!isOn)
+        {
+            return;
+        }
+
+        audioSource.PlayOneShot(tvSound);
+
+        bool isScreen1Active = tvScreen1.activeSelf; // nosaka, vai bilde ir aktīva
+
+        tvScreen1.SetActive(!isScreen1Active);
+        tvScreen2.SetActive(isScreen1Active);
+    }
+
+    private void channelButtonVisible()
+    {
+        if (channelButton != null)
+        {
+            channelButton.SetActive(isOn);
         }
     }
 }
